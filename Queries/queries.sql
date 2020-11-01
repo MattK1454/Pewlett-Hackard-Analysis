7f312a107debf06e44a58ee5f8b4b276ab711ecf
MattK <MattK1454@users.noreply.github.com>
2020-11-01 16:54:22 -0600

    Pewlett-Hackard list for retirement eligible employees added.

--- Queries/queries.sql
+++ Queries/queries.sql
@@ -35,3 +35,102 @@ INTO retirement_info
 FROM employees
 WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 	AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+-- Joining departments and dept_manager tables
+SELECT departments.dept_name,
+	dept_manager.emp_no,
+	dept_manager.from_date,
+	dept_manager.to_date
+FROM departments
+INNER JOIN dept_manager
+	ON departments.dept_no = dept_manager.dept_no;
+
+-- Joining retirement_info and dept_emp tables
+SELECT ri.emp_no,
+	ri.first_name,
+	ri.last_name,
+	de.to_date
+FROM retirement_info as ri
+LEFT JOIN dept_employees de
+	ON ri.emp_no = de.emp_no;
+	
+-- Joining departments and dept_manager tables
+SELECT d.dept_name,
+	dm.emp_no,
+	dm.from_date,
+	dm.to_date
+FROM departments AS d
+INNER JOIN dept_manager AS dm
+	ON d.dept_no = dm.dept_no;
+	
+SELECT ri.emp_no,
+	ri.first_name,
+	ri.last_name,
+	de.to_date
+INTO current_emp
+FROM retirement_info AS ri
+LEFT JOIN dept_employees AS de
+	ON ri.emp_no = de.emp_no
+WHERE de.to_date = ('9999-01-01');
+
+SELECT e.emp_no, 
+	e.first_name, 
+	e.last_name, 
+	e.gender,
+	s.salary,
+	de.to_date
+INTO emp_info
+FROM employees AS e
+INNER JOIN salaries AS s
+	ON (e.emp_no = s.emp_no)
+INNER JOIN dept_employees AS de
+	ON (e.emp_no = de.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+	AND (e.hire_date BETWEEN '1985-01-01' AND '1988-12-31')
+	AND (de.to_date = '9999-01-01');
+	
+--List of managers per department
+SELECT dm.dept_no,
+	   d.dept_name,
+	   dm.emp_no,
+	   ce.last_name,
+	   ce.first_name,
+	   dm.from_date,
+	   dm.to_date
+INTO manager_info
+FROM dept_manager AS dm
+	INNER JOIN departments AS d
+		ON (dm.dept_no = d.dept_no)
+	INNER JOIN current_emp AS ce
+		ON (dm.emp_no = ce.emp_no);
+		
+SELECT ce.emp_no,
+	   ce.first_name,
+	   ce.last_name,
+	   d.dept_name
+INTO dept_info
+FROM current_emp as ce
+	INNER JOIN dept_employees as de
+		ON (ce.emp_no = de.emp_no)
+	INNER JOIN departments AS d	
+		ON (de.dept_no = d.dept_no);
+
+-- Filter table for Sales team data
+SELECT ce.emp_no,
+	   ce.first_name,
+	   ce.last_name,
+	   di.dept_name
+INTO sales_dept_info
+FROM current_emp AS ce
+INNER JOIN dept_info AS di
+	ON (ce.emp_no = di.emp_no)
+WHERE (di.dept_name = 'Sales');
+
+SELECT ri.emp_no,
+	   ri.first_name,
+	   ri.last_name,
+	   di.dept_name
+FROM retirement_info AS ri
+INNER JOIN dept_info AS di
+	ON (ri.emp_no = di.emp_no)
+WHERE (dept_name = 'Sales');
--- schema.sql
+++ schema.sql
@@ -51,3 +51,12 @@ CREATE TABLE titles (
 	to_date DATE NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
 );
+
+-- Create new table for retiring employees
+SELECT emp_no, first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+	AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+-- Check the table
+SELECT * FROM retirement_info;
