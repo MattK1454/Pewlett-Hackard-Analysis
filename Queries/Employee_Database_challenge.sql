d488465a0c311721ca7055fc919b97ffdf466a21
MattK <69433858+MattK1454@users.noreply.github.com>
2020-11-01 20:07:02 -0600

    Add files via upload

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -28,3 +28,32 @@ INTO retiring_titles
 FROM unique_titles
 GROUP BY title
 ORDER BY "count" DESC;
+
+--Create mentorship eligibility table
+SELECT DISTINCT ON (e.emp_no) e.emp_no,
+	   e.first_name,
+	   e.last_name,
+	   e.birth_date,
+	   de.from_date,
+	   de.to_date,
+	   ti.title
+INTO mentorship_eligibility
+FROM employees AS e
+INNER JOIN dept_employees AS de
+	ON (e.emp_no = de.emp_no)
+INNER JOIN titles AS ti
+	ON (e.emp_no = ti.emp_no)
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+	AND (de.to_date = '9999-01-01')
+ORDER BY emp_no;
+
+--Sorting for mentors to develop training programs
+SELECT ri.emp_no,
+	   ri.first_name,
+	   ri.last_name,
+	   ti.title
+INTO manager_key_points
+FROM retirement_info AS ri
+INNER JOIN titles AS ti
+	ON (ri.emp_no = ti.emp_no)
+WHERE (ti.title = 'Manager');
